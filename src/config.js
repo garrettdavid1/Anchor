44126ef1984d7d08d02fb86047e2b22bdfef03af
David Garrett <garrettdavid1@gmail.com>
2018-02-04 11:56:45 -0500

    Removed localhost apiEndpointDomain to see if it will force the heroku config var

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 export const config = {
-    apiEndpointDomain: process.env['API_ENDPOINT_DOMAIN'] || 'http://localhost:1337'
+    apiEndpointDomain: process.env['API_ENDPOINT_DOMAIN']
+    //  || 'http://localhost:1337'
 }

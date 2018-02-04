1ae6ceb1d60ca7324d2029b7ec7d964225179a18
David Garrett <garrettdavid1@gmail.com>
2018-02-04 14:41:40 -0500

    Triggering new buildpack

--- src/config.js
+++ src/config.js
@@ -2,4 +2,5 @@ export const config = {
     apiEndpointDomain: 'https://peaceful-woodland-61112.herokuapp.com'
     // apiEndpointDomain: process.env['API_ENDPOINT_DOMAIN']
     // apiEndpointDomain: 'http://localhost:1337'
+    // useless comment
 }

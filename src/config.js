683b6caba5f269f41993690ea614ea6c4133df30
David Garrett <garrettdavid1@gmail.com>
2018-02-06 22:50:26 -0500

    Set up handling of api endpoint based off environment

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ import {lib} from './helpers/lib';
 
 export const config = {
     // apiEndpointDomain: 'https://peaceful-woodland-61112.herokuapp.com'
-    // apiEndpointDomain: process.env['API_ENDPOINT_DOMAIN']
-    apiEndpointDomain: lib.exists(process.env['API_ENDPOINT_DOMAIN']) ? process.env['API_ENDPOINT_DOMAIN'] : 'http://localhost:1337'
+    apiEndpointDomain: process.env['REACT_APP_API_ENDPOINT_DOMAIN']
+    // apiEndpointDomain: lib.exists(process.env['API_ENDPOINT_DOMAIN']) ? process.env['API_ENDPOINT_DOMAIN'] : 'http://localhost:1337'
 }
